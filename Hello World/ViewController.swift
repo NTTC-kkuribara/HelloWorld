cc19707f19912a445f1c10288d0b44f21029acdf
NTTC-kkuribara <kattun.kuribara@ntt.com>
2021-07-14 18:16:16 +0900

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by ITMS NTTcom on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
